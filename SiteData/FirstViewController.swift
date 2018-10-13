3353b3849cc81c04c30cb99585d339869108d05f
Mark Lyons <lyons340@cpe-2607-fcc8-9dc9-ad00-1094-21dc-41e2-fbf7.dyn6.twc.com>
2018-10-13 04:52:09 -0400

    Initial Commit

---
+++ SiteData/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SiteData
+//
+//  Created by Mark Lyons on 10/13/18.
+//  Copyright © 2018 Mark Lyons. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SiteData/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SiteData
+//
+//  Created by Mark Lyons on 10/13/18.
+//  Copyright © 2018 Mark Lyons. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
